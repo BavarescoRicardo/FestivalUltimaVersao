f8a7515b37d02817889bce7c452dc6fe42c1e019
ricardo <ricardo>
2020-07-08 21:48:55 -0300

    bancoo

--- desktop/Formularios/CantorFrm.Designer.cs
+++ desktop/Formularios/CantorFrm.Designer.cs
@@ -408,7 +408,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 470);
+            this.ClientSize = new System.Drawing.Size(719, 486);
             this.Controls.Add(this.layoutControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CantorFrm";
