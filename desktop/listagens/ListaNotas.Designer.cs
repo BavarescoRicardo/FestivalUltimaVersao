6f3ff896703d4dd8ddfa4c10adcce777b545bacb
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-07-19 19:28:31 -0300

    adicionado filtro as notas

--- desktop/listagens/ListaNotas.Designer.cs
+++ desktop/listagens/ListaNotas.Designer.cs
@@ -39,23 +39,25 @@
             this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.cmbFiltro = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbFiltro.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl
             // 
             this.gridControl.DataSource = this.bindingSource;
-            this.gridControl.Location = new System.Drawing.Point(12, -2);
+            this.gridControl.Location = new System.Drawing.Point(13, 8);
             this.gridControl.MainView = this.gridView1;
             this.gridControl.Name = "gridControl";
             this.gridControl.Size = new System.Drawing.Size(860, 366);
             this.gridControl.TabIndex = 0;
             this.gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
-            this.gridControl.Click += new System.EventHandler(this.gridControl_Click);
             // 
             // bindingSource
             // 
@@ -71,6 +73,7 @@
             this.gridColumn2,
             this.gridColumn3});
             this.gridView1.GridControl = this.gridControl;
+            this.gridView1.GroupPanelText = "Notas";
             this.gridView1.Name = "gridView1";
             this.gridView1.SortInfo.AddRange(new DevExpress.XtraGrid.Columns.GridColumnSortInfo[] {
             new DevExpress.XtraGrid.Columns.GridColumnSortInfo(this.colSequencial, DevExpress.Data.ColumnSortOrder.Ascending)});
@@ -81,6 +84,8 @@
             this.colSequencial.Caption = "Codigo";
             this.colSequencial.FieldName = "id_notas";
             this.colSequencial.Name = "colSequencial";
+            this.colSequencial.OptionsFilter.AllowAutoFilter = false;
+            this.colSequencial.OptionsFilter.AllowFilter = false;
             this.colSequencial.Visible = true;
             this.colSequencial.VisibleIndex = 0;
             this.colSequencial.Width = 70;
@@ -89,6 +94,8 @@
             // 
             this.colcategoria.FieldName = "cantor";
             this.colcategoria.Name = "colcategoria";
+            this.colcategoria.OptionsFilter.AllowAutoFilter = false;
+            this.colcategoria.OptionsFilter.AllowFilter = false;
             this.colcategoria.Visible = true;
             this.colcategoria.VisibleIndex = 1;
             this.colcategoria.Width = 227;
@@ -97,6 +104,8 @@
             // 
             this.coldia.FieldName = "categoria";
             this.coldia.Name = "coldia";
+            this.coldia.OptionsFilter.AllowAutoFilter = false;
+            this.coldia.OptionsFilter.AllowFilter = false;
             this.coldia.Visible = true;
             this.coldia.VisibleIndex = 2;
             this.coldia.Width = 156;
@@ -105,6 +114,8 @@
             // 
             this.gridColumn1.FieldName = "jurado";
             this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsFilter.AllowAutoFilter = false;
+            this.gridColumn1.OptionsFilter.AllowFilter = false;
             this.gridColumn1.Visible = true;
             this.gridColumn1.VisibleIndex = 3;
             this.gridColumn1.Width = 149;
@@ -113,6 +124,8 @@
             // 
             this.gridColumn2.FieldName = "apresentacao";
             this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.OptionsFilter.AllowAutoFilter = false;
+            this.gridColumn2.OptionsFilter.AllowFilter = false;
             this.gridColumn2.Visible = true;
             this.gridColumn2.VisibleIndex = 4;
             this.gridColumn2.Width = 149;
@@ -121,15 +134,39 @@
             // 
             this.gridColumn3.FieldName = "nota1";
             this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.OptionsFilter.AllowAutoFilter = false;
+            this.gridColumn3.OptionsFilter.AllowFilter = false;
             this.gridColumn3.Visible = true;
             this.gridColumn3.VisibleIndex = 5;
             this.gridColumn3.Width = 97;
+            // 
+            // cmbFiltro
+            // 
+            this.cmbFiltro.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.cmbFiltro.Location = new System.Drawing.Point(198, 18);
+            this.cmbFiltro.Name = "cmbFiltro";
+            this.cmbFiltro.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbFiltro.Size = new System.Drawing.Size(161, 20);
+            this.cmbFiltro.TabIndex = 1;
+            this.cmbFiltro.SelectedValueChanged += new System.EventHandler(this.cmbFiltro_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Categoria";
             // 
             // ListaNotas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 376);
+            this.ClientSize = new System.Drawing.Size(881, 382);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbFiltro);
             this.Controls.Add(this.gridControl);
             this.Name = "ListaNotas";
             this.Text = "Lista das Notas";
@@ -137,6 +174,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbFiltro.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,5 +191,7 @@
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
         private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbFiltro;
+        private System.Windows.Forms.Label label1;
     }
 }
--- desktop/listagens/ListaNotas.cs
+++ desktop/listagens/ListaNotas.cs
@@ -1,4 +1,5 @@
 ﻿using Festival.bo;
+using Festival.or;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,54 +14,79 @@ namespace Festival.listagens
 {
     public partial class ListaNotas : Form
     {
+        IList<Festival.or.Notas> lista;
         public ListaNotas()
         {
             InitializeComponent();
 
             NotasBo bo = new NotasBo();
-            IList<Festival.or.Notas> lista = bo.Listar();
+            lista = bo.Listar();
             gridControl.RefreshDataSource();
 
             this.bindingSource.Clear();
             bindingSource.DataSource = lista;
+            // bindingSource.DataSource = lista.Where(x => x.categoria == 6);
             this.bindingSource.ResetBindings(true);
             gridControl.Refresh();
+
+            // Carregar filtro
+            CategoriaBo catBo = new CategoriaBo();
+            foreach (Categoria cat in catBo.Listar())
+            {
+                this.cmbFiltro.Properties.Items.Add(cat);
+            }
         }
 
-        private void gridControl_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void gridView1_CustomColumnDisplayText(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs e)
         {
-            if (e.Column.FieldName == "cantor")
+            try
             {
-                // Inicia objeto do banco caso esteja na coluna certa
-                CantorBo bo = new CantorBo();
-                // Localize objeto pelo id e substitui na coluna
-                e.DisplayText = bo.RetornePeloId((int)e.Value).nome;
-            }
+                if (e.Column.FieldName == "cantor")
+                {
 
-            if (e.Column.FieldName == "categoria")
-            {
-                CategoriaBo bo = new CategoriaBo();
-                e.DisplayText = bo.RetornePeloId((int)e.Value).categoria;
-            }
+                    // Inicia objeto do banco caso esteja na coluna certa
+                    CantorBo bo = new CantorBo();
+                    // Localize objeto pelo id e substitui na coluna
+                    e.DisplayText = bo.RetornePeloId((int)e.Value).nome;
 
-            if (e.Column.FieldName == "apresentacao")
-            {
-                // Inicia objeto do banco caso esteja na coluna certa
-                ApresentacaoBo bo = new ApresentacaoBo();
-                // Localize objeto pelo id e substitui na coluna
-                e.DisplayText = bo.RetornePeloId((int)e.Value).musica;
-            }
+                }
 
-            if (e.Column.FieldName == "jurado")
-            {
-                JuradoBo bo = new JuradoBo();
-                e.DisplayText = bo.RetornePeloId((int)e.Value).nome;
+                if (e.Column.FieldName == "categoria")
+                {
+                    CategoriaBo bo = new CategoriaBo();
+                    e.DisplayText = bo.RetornePeloId((int)e.Value).categoria;
+                }
+
+                if (e.Column.FieldName == "apresentacao")
+                {
+                    // Inicia objeto do banco caso esteja na coluna certa
+                    ApresentacaoBo bo = new ApresentacaoBo();
+                    // Localize objeto pelo id e substitui na coluna
+                    e.DisplayText = bo.RetornePeloId((int)e.Value).musica;
+                }
+
+                if (e.Column.FieldName == "jurado")
+                {
+                    JuradoBo bo = new JuradoBo();
+                    e.DisplayText = bo.RetornePeloId((int)e.Value).nome;
+                }
             }
+            catch (Exception)
+            {
+                MessageBox.Show("Filtro não pode ser realizado");
+                //return;
+                throw;
+            }
+            
+        }
+
+        private void cmbFiltro_SelectedValueChanged(object sender, EventArgs e)
+        {
+            gridView1.ActiveFilterString.Equals(cmbFiltro.SelectedText);
+
+            Categoria cat = (Categoria)cmbFiltro.SelectedItem;
+            bindingSource.DataSource = lista.Where(x => x.categoria == cat.id_categoria);
         }
     }
 }
